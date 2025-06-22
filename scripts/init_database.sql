93fd11691bc8e3f94336e2dea0eebdb96ccad031
Afrijal Rasya Putra <afrijalrasyap@gmail.com>
2025-06-23 01:26:46 +0800

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,27 @@
+*/
+  
+Use master;
+GO
 
+-- Menghapus dan membuat ulang database'Datawarehouse'
+CREATE DATABASE DataWarehouse;
+IF EXIST (SELECT 1 FROM sys.databases WHERE name = 'Datawarehouse')
+  BEGIN
+    ALTER DATBASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+  END;
+GO
+
+-- Membuate database 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+GO
+  
+use DataWarehouse;
+
+-- Membuat Schema
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA Silver;
+GO
+CREATE SCHEMA gold;
+GO
