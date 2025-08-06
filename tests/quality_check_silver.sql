5c9aedad9331c38f63030bbc02f532a05a9aee9f
Afrijal Rasya Putra <afrijalrasyap@gmail.com>
2025-08-06 16:13:24 +0800

    Update quality_check_silver.sql

--- tests/quality_check_silver.sql
+++ tests/quality_check_silver.sql
@@ -1,29 +1,27 @@
 /*
 ===============================================================================
-Checking data quality
+Quality Checks
 ===============================================================================
-Script purpose:
-    This script performs data quality checks such as consistency, accuracy, 
- and data standardization on the 'silver' layer. The checks performed include:
-    - Empty or duplicate primary keys.
-    - Unwanted spaces in strings.
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
     - Data standardization and consistency.
-    - Invalid date ranges and sequences.
+    - Invalid date ranges and orders.
     - Data consistency between related fields.
 
-Usage :
-    - Run this check after the data has been loaded into the Silver layer.
-    - Look for and resolve any discrepancies found during the check.
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
 
-
-
 -- ====================================================================
 -- Checking 'silver.crm_cust_info'
--- ======================================================================
--- Checking for empty or duplicate primary keys
--- Expected: No results
+-- ====================================================================
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
 SELECT 
     cst_id,
     COUNT(*) 
@@ -31,23 +29,23 @@ FROM silver.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
--- Memeriksa spasi yang tidak diinginkan
--- Ekspektasi: Tidak ada hasil
+-- Check for Unwanted Spaces
+-- Expectation: No Results
 SELECT 
     cst_key 
 FROM silver.crm_cust_info
 WHERE cst_key != TRIM(cst_key);
 
--- Standarisasi dan konsistensi data
+-- Data Standardization & Consistency
 SELECT DISTINCT 
     cst_marital_status 
 FROM silver.crm_cust_info;
 
 -- ====================================================================
--- Mengecek 'silver.crm_prd_info'
+-- Checking 'silver.crm_prd_info'
 -- ====================================================================
--- Memeriksa Primar key yang kosong atau duplikat
--- Ekspektasi: Tidak ada hasil
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
 SELECT 
     prd_id,
     COUNT(*) 
@@ -55,7 +53,7 @@ FROM silver.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL;
 
--- Memeriksa spasi yang tidak diinginkan
+-- Check for Unwanted Spaces
 -- Expectation: No Results
 SELECT 
     prd_nm 
@@ -63,29 +61,29 @@ FROM silver.crm_prd_info
 WHERE prd_nm != TRIM(prd_nm);
 
 -- Check for NULLs or Negative Values in Cost
--- Ekspektasi: Tidak ada hasil
+-- Expectation: No Results
 SELECT 
     prd_cost 
 FROM silver.crm_prd_info
 WHERE prd_cost < 0 OR prd_cost IS NULL;
 
--- Standarisasi dan konsistensi data
+-- Data Standardization & Consistency
 SELECT DISTINCT 
     prd_line 
 FROM silver.crm_prd_info;
 
--- Memeriksa rentan data yang tidak valid (Start Date > End Date)
--- Ekspektasi: Tidak ada hasil
+-- Check for Invalid Date Orders (Start Date > End Date)
+-- Expectation: No Results
 SELECT 
     * 
 FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
 
 -- ====================================================================
--- Mengecek 'silver.crm_sales_details'
+-- Checking 'silver.crm_sales_details'
 -- ====================================================================
--- Memeriksa tanggal yang tidak valid
--- Ekspektasi: Tidak ada hasil (artinya semua tanggal valid)
+-- Check for Invalid Dates
+-- Expectation: No Invalid Dates
 SELECT 
     NULLIF(sls_due_dt, 0) AS sls_due_dt 
 FROM bronze.crm_sales_details
@@ -94,16 +92,16 @@ WHERE sls_due_dt <= 0
     OR sls_due_dt > 20500101 
     OR sls_due_dt < 19000101;
 
--- Mengecek Date order yang tidak valid(Order Date > Shipping/Due Dates)
--- Ekspektasi: tidak ada hasil
+-- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
+-- Expectation: No Results
 SELECT 
     * 
 FROM silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt 
    OR sls_order_dt > sls_due_dt;
 
--- Memeriksa konsistensi Data: Sales = Quantity * Price
--- Ekspektasi: Tidak ada hasil
+-- Check Data Consistency: Sales = Quantity * Price
+-- Expectation: No Results
 SELECT DISTINCT 
     sls_sales,
     sls_quantity,
@@ -119,34 +117,34 @@ WHERE sls_sales != sls_quantity * sls_price
 ORDER BY sls_sales, sls_quantity, sls_price;
 
 -- ====================================================================
--- Memeriksa 'silver.erp_cust_az12'
+-- Checking 'silver.erp_cust_az12'
 -- ====================================================================
--- Identifikasi tanggal di luar rentang
--- Ekspektasi: tanggal lahir antara 1924-01-01 dan Hari ini
+-- Identify Out-of-Range Dates
+-- Expectation: Birthdates between 1924-01-01 and Today
 SELECT DISTINCT 
     bdate 
 FROM silver.erp_cust_az12
 WHERE bdate < '1924-01-01' 
    OR bdate > GETDATE();
 
--- Standarisasi dan Konsistensi data
+-- Data Standardization & Consistency
 SELECT DISTINCT 
     gen 
 FROM silver.erp_cust_az12;
 
 -- ====================================================================
--- Memeriksa 'silver.erp_loc_a101'
+-- Checking 'silver.erp_loc_a101'
 -- ====================================================================
--- Standarisasi dan Konsistensi data
+-- Data Standardization & Consistency
 SELECT DISTINCT 
     cntry 
 FROM silver.erp_loc_a101
 ORDER BY cntry;
 
 -- ====================================================================
--- Mengecek 'silver.erp_px_cat_g1v2'
+-- Checking 'silver.erp_px_cat_g1v2'
 -- ====================================================================
--- Memeriksa spasi yang tidak diinginkan
+-- Check for Unwanted Spaces
 -- Expectation: No Results
 SELECT 
     * 
@@ -155,8 +153,7 @@ WHERE cat != TRIM(cat)
    OR subcat != TRIM(subcat) 
    OR maintenance != TRIM(maintenance);
 
--- Standarisasi dan konsitensi data
+-- Data Standardization & Consistency
 SELECT DISTINCT 
     maintenance 
 FROM silver.erp_px_cat_g1v2;
-
